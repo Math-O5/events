dcd2e83acbd62b7b42c43cada896e12064b07cc5
Mathias Fernandes Duarte Coelho <mathias.coelho@opus-software.com.br>
2023-04-10 00:25:22 -0300

    add custom query from database

--- back-events/back-events/src/main/java/com/event/backevents/domain/repository/EventEditionRepository.java
+++ back-events/back-events/src/main/java/com/event/backevents/domain/repository/EventEditionRepository.java
@@ -1,10 +1,17 @@
 package com.event.backevents.domain.repository;
 
 import com.event.backevents.domain.model.EventEdition;
+import com.event.backevents.domain.model.Location;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EventEditionRepository extends JpaRepository<EventEdition, Long> {
     Optional<EventEdition> findByName(String name);
+
+    @Query(value="SELECT * FROM EVENT_EDITION E WHERE E.", nativeQuery = true)
+    Optional<List<EventEdition>> findAllByLocation(Location location, Pageable pageble);
 }
